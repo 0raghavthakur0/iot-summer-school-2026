227826f11f3bccfdc435e54351e0341734cfce74
0raghavthakur0 <thakurraghavish5@gmail.com>
2026-07-04 14:57:15 +0530

    feat: control blink speed using potentiometer
    Added a count variable to track blink cycles and print to Serial.

--- week1/versioned_blink/versioned_blink.ino
+++ week1/versioned_blink/versioned_blink.ino
@@ -1,5 +1,8 @@
+int count = 0;
+
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -8,4 +11,8 @@ void loop() {
 
   digitalWrite(13, LOW);
   delay(500);
+
+  count++;
+  Serial.print("Blink count: ");
+  Serial.println(count);
 }
